c6495ceb447e56203b6bb2c41fc7fec605ed0c1c
Shayden Martin <shayden.martin@qantashotels.com>
2023-12-06 11:43:54 -0300

    Day 04, Part 2

--- solutions/day_04.clj
+++ solutions/day_04.clj
@@ -49,9 +49,49 @@
          (map (comp int #(math/pow 2 %) dec count))
          (reduce +))))
 
+;; Part 2 Solution Strategry
+;; 1. Set total cards to count of scratch cards
+;; 2. Finding winning cards and record the card-no & win count
+;; 3. Transform card-no & win count into list of card-nos
+;; 4. Count card-nos and add them to total cards 
+;; 5. If no card-nos continue, otherwise map card-nos to cards and recur to (2).
+;; 6. Return total winning cards
+
+(defn winning-nos [scratchcard]
+  (let [win-nos (-> scratchcard :win-nos set)
+        my-nos (-> scratchcard :my-nos set)]
+    (set/intersection win-nos my-nos)))
+
+(defn winning-cards [cards]
+  (map #(assoc % :winning-nos (winning-nos %))
+       cards))
+
+(defn winning-cards->card-nos [winning-cards]
+  (mapcat (fn [{:keys [no winning-nos]}]
+            (take (count winning-nos) (iterate inc (inc no))))
+          winning-cards))
+
+(defn card-nos->cards [scratchcards card-nos]
+  (map (into {} (map (juxt :no identity)) scratchcards)
+       card-nos))
+
+(defn ->find-new-cards [scratchcards]
+  (comp (partial card-nos->cards scratchcards)
+        winning-cards->card-nos
+        winning-cards))
+
 (defn part-two [& _args]
-  (let [input (-> input-path io/resource io/reader)]
-    ))
+  (let [input (-> input-path io/resource io/reader)
+        lines (line-seq input)
+        scratchcards (map parse-scratchcard lines)
+        find-new-cards (->find-new-cards scratchcards)]
+    (loop [total-cards (count scratchcards)
+           cards scratchcards]
+      (if (empty? cards)
+        total-cards
+        (let [new-cards (find-new-cards cards)]
+          (recur (+ total-cards (count new-cards))
+                 new-cards))))))
 
 (comment
   (part-one)
