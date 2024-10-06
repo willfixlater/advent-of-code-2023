5539faf69813201089bbd3327a4ef5a1746ee5b9
Shayden Martin <shayden.martin@qantashotels.com>
2024-10-07 02:59:36 +0930

    Update solution template with planning comments

--- solutions/day_XX.clj
+++ solutions/day_XX.clj
@@ -2,6 +2,19 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
+;; Input Grammar
+;; example -> something " " something-else
+;; something-else -> something " else"
+;; something -> string
+;; where string is a clojure string literal
+
+;; Sheet Spec
+;; {:example [something something-else]}
+
+;; Part 1 Solution Strategry
+;; 1. Example strategy step
+;; 2. Something in the second step
+
 (def input-path (str (str/replace *ns* #"-" "_") ".txt"))
 
 (defn part-one [& _args]
